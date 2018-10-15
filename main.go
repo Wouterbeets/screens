0e7fa1b217bdd767a87a830185a55a786b239e56
wouterbeets <beetswouter@gmail.com>
2018-10-15 22:59:50 +0200

    generalised lib

--- main.go
+++ main.go
@@ -13,24 +13,44 @@ func main() {
 	flag.Parse()
 	args := flag.Args()
 	if len(args) != 1 {
+		fmt.Println("error: no id")
 		os.Exit(1)
 	}
-	c, err := cyclist.New(map[string]cyclist.Cycle{
-		"0": cyclist.Cycle{
-			Entries: []cyclist.Entry{
-				{Cmd: "xrandr --auto"},
+	c, err := cyclist.New(
+		map[string]cyclist.Cycle{
+			"0": cyclist.Cycle{
+				Entries: []string{
+					"xrandr --auto",
+				},
 			},
 		},
-	})
+		os.Getenv("HOME")+"/.screens",
+	)
 	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
+
 	nbScreens := args[0]
 	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
+
 	cmd, err := c.Cycle(nbScreens)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	comm := exec.Command("notify-send", cmd)
+	err = comm.Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	comm = exec.Command(cmd)
 	err = comm.Run()
 	if err != nil {
 		fmt.Println(err)
